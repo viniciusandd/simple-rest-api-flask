25d64f00cecc8dd907377168e002b9fa30b4ed51
viniciusandd <viniciusemanoelandrade@hotmail.com>
2019-02-06 23:39:05 -0200

    Retornando um json com itens serializados pela lib jsonify

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
 
 app = Flask(__name__)
 
 # Raiz da URL
-@app.route("/")
-def hello():
+@app.route("/", methods=['GET'])
+def inicio():
     return "Servidor iniciado com sucesso."
+
+produtos = [
+    {
+        'codigo': '01',
+        'descricao': 'Mouse'
+    },
+    {
+        'codigo': '02',
+        'descricao': 'Monitor'
+    },
+    {
+        'codigo': '03',
+        'descricao': 'Notebook'
+    } ,
+    {
+        'codigo': '04',
+        'descricao': 'Cooler'
+    }             
+]
+
+@app.route("/produtos", methods=['GET'])
+def get_produtos():
+    return jsonify(produtos)
 
 if __name__ == "__main__":
     app.run(debug=True)
